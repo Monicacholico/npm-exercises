99683b1547630f1b151b53a33380ce8099657276
monicacholico <monicachol@hotmail.com>
2018-04-23 15:17:17 -0500

    "finished npm exercises"

---
+++ src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const $ = require('jquery');
+
+const sayHello = name => 'Hello, ' + name + '!';
+console.log(sayHello);
+sayHello();
+
+$('h1').css('background-color', 'yellow');
+
+
